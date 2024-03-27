e317f4c4127bf190b18e9d96e531709e550a8dfc
hearthacker-cyber <hearthacks2020@gmail.com>
2024-03-27 22:25:58 +0530

    Your msg

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -123,16 +123,16 @@
 
             <!-- Nav Item - Charts -->
             <li class="nav-item">
-                <a class="nav-link" href="charts.html">
+                <a class="nav-link" href="import.php">
                     <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
+                    <span>Import file</span></a>
             </li>
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
                 <a class="nav-link" href="tables.php">
                     <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
+                    <span>Tables</span></a> 
             </li>
 
             <!-- Divider -->
